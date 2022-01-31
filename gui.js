2e5e34ea0ad9a9f5a3908e171cd750417cd3d968
SBrick <samuelbrick3@gmail.com>
2022-01-31 13:44:22 -0600

    Added css animations and task info box rendering function

--- gui.js
+++ gui.js
@@ -14,7 +14,93 @@ const essentialTaskBar = document.createElement("div");
 const majorTaskBar = document.createElement("div");
 const minorTaskBar = document.createElement("div");
 
+const taskInfoBox = document.createElement("div");
+const dueDateDisplay = document.createElement("p");
+const descriptionDisplay = document.createElement("p");
+const editBtn = document.createElement("button");
 
+
+function clearTaskInfoBox() {
+    while (taskInfoBox.firstChild) {
+        taskInfoBox.removeChild(taskInfoBox.firstChild);
+    };
+    body.removeChild(taskInfoBox);
+};
+
+function renderTaskInfoBox() {
+    if (taskInfoBox.firstChild) {clearTaskInfoBox()};
+    taskInfoBox.classList.add("task-info-box");
+
+    dueDateDisplay.classList.add("due-date-display");
+    dueDateDisplay.textContent = "Due Date: 6:00AM XX/XX/XXXX";
+    taskInfoBox.appendChild(dueDateDisplay);
+
+    descriptionDisplay.classList.add("description-display");
+    descriptionDisplay.textContent = "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Harum iusto distinctio, ex quos quo molestias molestiae? Neque accusantium culpa perferendis, non nulla aperiam eaque magnam omnis ipsam beatae, deleniti porro? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellat repudiandae quam iure. Voluptatum omnis eveniet iste, dolor adipisci culpa obcaecati placeat veniam aspernatur tempore aliquid nemo dicta magnam, est reprehenderit?";
+    taskInfoBox.appendChild(descriptionDisplay);
+
+    editBtn.classList.add("edit-btn");
+    editBtn.textContent = "Edit";
+    taskInfoBox.appendChild(editBtn);
+
+    body.appendChild(taskInfoBox);
+};
+
+
+function clearTaskLibraryBox() {
+    while (essentialTaskBar.firstChild) {
+        essentialTaskBar.removeChild(essentialTaskBar.firstChild);
+    };
+    while (majorTaskBar.firstChild) {
+        majorTaskBar.removeChild(majorTaskBar.firstChild);
+    };
+    while (minorTaskBar.firstChild) {
+        minorTaskBar.removeChild(minorTaskBar.firstChild);
+    };
+    while (taskLibraryBox.firstChild) {
+        taskLibraryBox.removeChild(taskLibraryBox.firstChild);
+    };
+    body.removeChild(taskLibraryBox);
+};
+
+function renderTaskLibraryBox() {
+    if (taskLibraryBox.firstChild) {clearTaskLibraryBox()};
+    
+    taskLibraryBox.classList.add("library-box", "task-library");
+
+    const taskBarArrays = [essentialTaskBar, majorTaskBar, minorTaskBar, "Essential", "Major", "Minor"];
+    for (let i = 0; i < 3; i++) {
+        taskBarArrays[i].classList.add("priority-box");
+        const taskRank = document.createElement("p");
+        taskRank.textContent =  `${taskBarArrays[i + 3]}`;
+        taskBarArrays[i].appendChild(taskRank);
+        const addTaskBtn = document.createElement("button");
+        addTaskBtn.classList.add("add-task-btn");
+        addTaskBtn.textContent = "+";
+        taskBarArrays[i].appendChild(addTaskBtn);
+        taskLibraryBox.appendChild(taskBarArrays[i]);
+
+        const taskLibrary = document.createElement("ul");
+        taskLibrary.classList.add(`${taskBarArrays[i + 3].toLowerCase()}-library`);
+        for (let i = 0; i < 3; i++) {
+            const task = document.createElement("li");
+            task.textContent = `${[i + 1]}`;
+            task.addEventListener("click", () => {
+                renderTaskInfoBox();
+            });
+            const bigCircle = document.createElement("div");
+            bigCircle.classList.add("big-circle");
+            const smallCircle = document.createElement("div");
+            smallCircle.classList.add("small-circle");
+            bigCircle.appendChild(smallCircle);
+            task.appendChild(bigCircle);
+            taskLibrary.appendChild(task);
+        };
+        taskLibraryBox.appendChild(taskLibrary);
+    };
+
+    body.appendChild(taskLibraryBox);
+};
 
 function renderHomeLibrayBox() {
     homeLibraryBox.classList.add("library-box");
@@ -26,7 +112,12 @@ function renderHomeLibrayBox() {
     projectLibrary.classList.add("project-library");
     for (let i = 0; i < 10; i++) {
         const project = document.createElement("li");
-        project.addEventListener("click", renderTaskLibraryBox);
+        project.addEventListener("click", () => {
+            if (taskInfoBox.firstChild) {
+                clearTaskInfoBox();
+            };
+            renderTaskLibraryBox()
+        });
         project.textContent = `Example ${[i]}`;
         projectLibrary.appendChild(project);
     };
@@ -47,38 +138,6 @@ function renderHomeLibrayBox() {
     body.appendChild(homeLibraryBox);
 };
 
-function renderTaskLibraryBox() {
-    taskLibraryBox.classList.add("library-box", "task-library");
 
-    const taskBarArrays = [essentialTaskBar, majorTaskBar, minorTaskBar, "Essential", "Major", "Minor"];
-    for (let i = 0; i < 3; i++) {
-        taskBarArrays[i].classList.add("priority-box");
-        const taskRank = document.createElement("p");
-        taskRank.textContent =  `${taskBarArrays[i + 3]}`;
-        taskBarArrays[i].appendChild(taskRank);
-        const addTaskBtn = document.createElement("button");
-        addTaskBtn.classList.add("add-task-btn");
-        addTaskBtn.textContent = "+";
-        taskBarArrays[i].appendChild(addTaskBtn);
-        taskLibraryBox.appendChild(taskBarArrays[i]);
-
-        const taskLibrary = document.createElement("ul");
-        taskLibrary.classList.add(`${taskBarArrays[i + 3].toLowerCase()}-library`);
-        for (let i = 0; i < 3; i++) {
-            const task = document.createElement("li");
-            task.textContent = `${[i + 3]}`;
-            const bigCircle = document.createElement("div");
-            bigCircle.classList.add("big-circle");
-            const smallCircle = document.createElement("div");
-            smallCircle.classList.add("small-circle");
-            bigCircle.appendChild(smallCircle);
-            task.appendChild(bigCircle);
-            taskLibrary.appendChild(task);
-        };
-        taskLibraryBox.appendChild(taskLibrary);
-    };
-
-    body.appendChild(taskLibraryBox);
-};
 
 renderHomeLibrayBox();
